8d0beca6649c91039e02ed9f236943eb4411df5b
”名前” <greatpotato20@gmail.com>
2019-07-18 13:21:34 +0000

    pictgram

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+　validates :password, length: {in: 8..32}, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z}
+  
+  has_secure_password
+end
