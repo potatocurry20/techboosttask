364e3a4a76586da8ddc64c820bbd36f20f6c8754
”名前” <greatpotato20@gmail.com>
2019-07-29 23:00:36 +0000

    secondtask

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  #名前は１５文字以上
   validates :name, presence: true, length: {maximum: 15}
-  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-　validates :password, length: {in: 8..32}, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z}
   
-  has_secure_password
+  #「~ @ ~ . ~」の形を許可
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX }
+  
+　#文字数を8~32文字のみ
+　#少なくともアルファベットと数字はそれぞれ1文字以上含まないといけない
+　has_secure_password
+　VALID_PASWAD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+　validates :password, presence: true, uniqueness: true, length: {in: 8..32}, format: {with: VALID_PASWAD_REGEX}
+  
 end
